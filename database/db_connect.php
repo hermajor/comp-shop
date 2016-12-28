2fec37134fb89528f91dfc59299a0e6071840fc6
Sergio Ramone <sergio.ramone451@gmail.com>
2016-12-28 13:23:14 +0300

    ignore test 3

--- database/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once ("BDinfo.php");
-
-$connection = mysqli_connect($host, $login, $password, $db);
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL:". mysqli_connect_error();
-}
